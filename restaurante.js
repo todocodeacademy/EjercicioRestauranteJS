bc9c677dc48a14927ac73d73c4986efd99bff441
todocodeacademy <admin@todocodeacademy.com>
2022-10-26 17:26:55 -0300

    commit1

---
+++ restaurante.js
@@ -0,0 +1,8 @@
+var totalComida = 2800 + 1300;
+var totalBebida = 900 + 650;
+
+var total = totalComida + totalBebida;
+
+console.log("El total de comida es: " + totalComida);
+console.log("El total de bebida es: " + totalBebida);
+console.log("El total de la cuenta es: " + total);
